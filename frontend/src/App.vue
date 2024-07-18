7088a928bd376e1f4f7bb73f0dac24f1e1ab36de
natnaelLake <bewuketulake1212@gmail.com>
2024-07-18 19:33:58 +0300

    first setup with vue 3

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import ExampleComponent from './components/ExampleComponent.vue'
+</script>
+
+<template>
+  <ExampleComponent />
+</template>
